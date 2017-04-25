661a31197609eb00ced0d40dc14e6827bbb7ffd2
Kevin <kevin_cyber@yahoo.com>
2017-04-25 10:30:35 +0800

    Fixed Endorser Email

--- resources/views/emails/endorser.blade.php
+++ resources/views/emails/endorser.blade.php
@@ -5,7 +5,7 @@
 		<h2>{{ config('preferences.notif_subjects.created') }}</h2>
 	</div>
 	<div class="content">
-	Hi, Mr./Ms. {{ $utils->getFName($loan->FullName) }}!
+	Hi, Mr./Ms. {{ $utils->getFName($loan->endorser_FullName) }}!
 	<br>
 	<br>
 	A new loan application has been created with control no.: {{ $loan->ctrl_no }}.
