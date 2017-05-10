659523289e7a3ec7fb1cb5f43066661667ce245a
Kevin <kevin_cyber@yahoo.com>
2017-05-10 09:31:18 +0800

    Fixed pagination

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
      */
     public function index(Request $request)
     {
-        $data = User::withoutGlobalScope('active')->orderBy('id','asc')->paginate(1000);
+        $data = User::withoutGlobalScope('active')->orderBy('id','asc')->paginate(10);
         return view('admin.users.index',compact('data'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
