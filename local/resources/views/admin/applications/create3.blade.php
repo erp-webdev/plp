333bbea99aeb7a6ccd44953355b2b96db69f56c7
Kevs <keven.ayag@adamson.edu.ph>
2022-02-08 15:02:53 +0800

    update

--- local/resources/views/admin/applications/create3.blade.php
+++ local/resources/views/admin/applications/create3.blade.php
@@ -2,17 +2,19 @@
 @section('content')
 
 <div class="container-fluid">
-    <table class="table">
-        <tbody>
-            <tr>
-                <td colspan="2">TYPE OF APPLICATION</td>
-            </tr>
-            <tr>
-                <td><input id="type" name="type" type="radio">New</td>
-                <td><input id="type" name="type" type="radio">Re-availment</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="col-md-6 col-sm-6"> 
+        <table class="table">
+            <tbody>
+                <tr>
+                    <td colspan="2">TYPE OF APPLICATION</td>
+                </tr>
+                <tr>
+                    <td><input id="type" name="type" type="radio">New</td>
+                    <td><input id="type" name="type" type="radio">Re-availment</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
 
 @endsection
