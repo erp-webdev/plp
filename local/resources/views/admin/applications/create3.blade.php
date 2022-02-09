5a6789bfcacf652080b967942733bd21cb7f477c
Kevs <keven.ayag@adamson.edu.ph>
2022-02-09 08:50:43 +0800

    update

--- local/resources/views/admin/applications/create3.blade.php
+++ local/resources/views/admin/applications/create3.blade.php
@@ -10,15 +10,15 @@
                         <td colspan="2"><h3>APPLICATION FORM</h3></td>
                     </tr>
                     <tr>
-                        <td><label for=""><input id="type" name="type" type="radio" value="0" {{ $records == 0 ? 'selected' : '' }} required> NEW</label> </td>
+                        <td><label for=""><input id="type" name="type" type="radio" value="0" {{ $records == 0 ? 'checked' : '' }} required> NEW</label> </td>
                         <td>
-                        <label for=""><input id="type" name="type" type="radio" value="1" {{ $records > 0 ? 'selected' : '' }} required> RE-AVAILMENT</label>    
+                        <label for=""><input id="type" name="type" type="radio" value="1" {{ $records > 0 ? 'checked' : '' }} required> RE-AVAILMENT</label>    
                             <br> <span>Previous loan amount: {{ number_format($previous_loan, 2, '.', ',') }}</span>
                             <br> <span>Balance: {{ number_format($balance, 2, '.', ',') }}</span>
                         </td>
                     </tr>
                     <tr>
-                        <td><label for=""><input type="radio" name="special" value="0" selected required> REGULAR</label></td>
+                        <td><label for=""><input type="radio" name="special" value="0" checked required> REGULAR</label></td>
                         <td><label for=""><input type="radio" name="special" value="1" required> SPECIAL</label></td>
                     </tr>
                     <tr>
@@ -37,7 +37,7 @@
                     </tr>
                     <tr>
                         <th>PURPOSE</th>
-                        <td><input type="text" class="form-control" name="purpose" value=""></td>
+                        <td><input type="text" class="form-control" name="purpose" value="" required></td>
                     </tr>
                     <tr>
                         <th>ENDORSED BY</th>
