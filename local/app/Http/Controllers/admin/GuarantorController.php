727fe7ebfdfab67fac199ccdb666a4f14a64aa6f
Kevs <keven.ayag@adamson.edu.ph>
2022-02-16 11:00:40 +0800

    update

--- local/app/Http/Controllers/admin/GuarantorController.php
+++ local/app/Http/Controllers/admin/GuarantorController.php
@@ -58,7 +58,7 @@ class GuarantorController extends Controller
             $guarantor = Guarantor::findOrFail($id);
             $loan = Loan::findOrFail($guarantor->eFundData_id);
             $employee = Employee::where('EmpID', $loan->EmpID)->first();
-            $terms = Terms::getRankLimits($employee->RankDesc);
+            $terms = Terms::getRankLimits($employee);
 
             $limits = GLimits::limit($guarantor->EmpID);
             
