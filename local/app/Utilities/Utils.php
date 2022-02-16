d746678d28ee0fca2475a6883343f1370ee794e1
Kevs <keven.ayag@adamson.edu.ph>
2022-02-16 10:43:54 +0800

    update

--- local/app/Utilities/Utils.php
+++ local/app/Utilities/Utils.php
@@ -135,7 +135,7 @@ class Utils
      * @return int 
      *
      */
-    public function setStatus($status = null, $withSurety = null)
+    public function setStatus($status, $withSurety = null)
     {
     	if($status == null){
     		return 0;
