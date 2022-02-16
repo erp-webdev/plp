a585ef2250eddafba066fc1c9d6f14804856f5e7
Kevs <keven.ayag@adamson.edu.ph>
2022-02-16 15:55:05 +0800

    update

--- local/resources/views/admin/validation/validation.blade.php
+++ local/resources/views/admin/validation/validation.blade.php
@@ -102,8 +102,8 @@
     <div class="modal-footer">
       <button type="button" class="btn btn-default btn-sm" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
       @if($loan->endorser_status == null && $loan->status == $utils->getStatusIndex('endorser'))
-      <button type="submit" name="deny" class="btn btn-danger btn-sm" onsubmit="startLoading()"><i class="fa fa-thumbs-down"></i> Deny</button>
-      <button type="submit" name="approve" class="btn btn-success btn-sm" onsubmit="startLoading()"><i class="fa fa-thumbs-up"></i> Approve</button>
+      <button type="submit" name="deny" class="btn btn-danger btn-sm" onsubmit="startLoading()"><i class="fa fa-thumbs-down"></i> Invalid</button>
+      <button type="submit" name="approve" class="btn btn-success btn-sm" onsubmit="startLoading()"><i class="fa fa-thumbs-up"></i> Valid</button>
       @endif
     </div>
   </form>
