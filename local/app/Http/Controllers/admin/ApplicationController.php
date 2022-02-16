042a80fb75c4ed671bc443a134a39368eb2887e6
Kevs <keven.ayag@adamson.edu.ph>
2022-02-16 10:48:18 +0800

    update

--- local/app/Http/Controllers/admin/ApplicationController.php
+++ local/app/Http/Controllers/admin/ApplicationController.php
@@ -379,8 +379,7 @@ class ApplicationController extends Controller
     public function create_loan(Request $request)
     {
         $loan = new Loan();
-        var_dump($request->id);
-        if(!empty($request->id))
+        if(!empty(trim($request->id)))
             $loan = Loan::find($request->id);
 
         // Interest percentage
