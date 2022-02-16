5d302776a47b1ef3b10a923644683d2d46908fa3
Kevs <keven.ayag@adamson.edu.ph>
2022-02-16 17:28:41 +0800

    update

--- local/app/Http/Controllers/admin/ValidationController.php
+++ local/app/Http/Controllers/admin/ValidationController.php
@@ -70,7 +70,7 @@ class ValidationController extends Controller
         if(isset($_POST['approve'])){
 
             $loan_validation = Loan::findOrFail($request->id);
-            if($loan_validation->EmpID != Auth::user()->employee_id)
+            if($loan_validation->EmpID == Auth::user()->employee_id)
                 abort(403);
 
             if(!empty(trim($loan_validation->company_nurse)))
