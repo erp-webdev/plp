420cb6c6fcb17f02a6aec58946c926b257037304
Kevs <keven.ayag@adamson.edu.ph>
2022-03-04 18:08:24 +0800

    update

--- local/app/Http/Controllers/admin/EmailController.php
+++ local/app/Http/Controllers/admin/EmailController.php
@@ -34,13 +34,15 @@ class EmailController extends Controller
             $from = config('preferences.email_from');
             $utils = new Utils();
 
-            Mail::send($body, ['employee' => $emp, 'args' => $args, 'utils' => $utils], function($message) use ($to, $subject, $from, $cc){
+            $mail = Mail::send($body, ['employee' => $emp, 'args' => $args, 'utils' => $utils], function($message) use ($to, $subject, $from, $cc){
                 $message->to('kayag.global@megaworldcorp.com');
                 // $message->to($to);
                 $message->from($from);
                 $message->subject($subject);
                 // $message->cc($cc);
             });
+
+            dd($mail);
 
             $log = new Log();
             $log->writeOnly('Info', 'email', ['email' => $to, 'subject' => $subject]);
