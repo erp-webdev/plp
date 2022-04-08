e37e5ce1273d705e0694425c386d31f32bddda1f
Kevs <keven.ayag@adamson.edu.ph>
2022-04-08 08:35:08 +0800

    update

--- local/resources/views/admin/loans/loanApproval.blade.php
+++ local/resources/views/admin/loans/loanApproval.blade.php
@@ -11,6 +11,9 @@
 		  		@endif
 		  	@endif
 	  		</h4>
+			@if(!empty(trim($loan->payroll_remarks)))
+			<span><strong>Remarks by Payroll: </strong> {{ $loan->payroll_remarks }}</span>
+			@endif
 	  </div>
 	  <div class="col-xs-12 col-sm-6 col-md-6">
 	  	<p class="pull-right"><small>Ctrl No: </small><strong >{{ $loan->ctrl_no }}</strong></p>
