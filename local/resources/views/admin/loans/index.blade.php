d394336a671e963415b772923ef0b2a169397e75
Kevs <keven.ayag@adamson.edu.ph>
2022-04-08 09:04:48 +0800

    update

--- local/resources/views/admin/loans/index.blade.php
+++ local/resources/views/admin/loans/index.blade.php
@@ -133,9 +133,9 @@
 					</div>
 					<div class="input-group col-xs-12 col-sm-3 col-md-3">
 						<select name="status" id="status" class="form-control">
-							<option value="all" {{ isset($_GET['status']) ? $_GET['status'] == 'all' ? 'selected' : '' : '' }}>Select all</option>
+							<option value="all" {{ isset($_GET['status']) ? $_GET['status'] === 'all' ? 'selected' : '' : '' }}>Select all</option>
 							@foreach($utils->stats as $index => $stat)
-							<option value="{{ $index }}" {{ isset($_GET['status']) ? $_GET['status'] == $index ? 'selected' : '' : '' }}>{{ $stat }}</option>
+							<option value="{{ $index }}" {{ isset($_GET['status']) ? $_GET['status'] === $index ? 'selected' : '' : '' }}>{{ $stat }}</option>
 							@endforeach
 						</select>
 						<a class="input-group-addon btn btn-success btn-sm" onclick="find()"><i class="fa fa-search"></i></a>
