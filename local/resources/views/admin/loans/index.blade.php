c2c5654d86b28a228fbecd3b2d91fa040f292b9f
Kevs <keven.ayag@adamson.edu.ph>
2022-04-08 09:13:56 +0800

    update

--- local/resources/views/admin/loans/index.blade.php
+++ local/resources/views/admin/loans/index.blade.php
@@ -68,6 +68,27 @@
       			</div><!-- /.modal-content -->
 		    </div><!-- /.modal-dialog -->
 		  </div><!-- /.modal -->
+		  <div class="modal fade" tabindex="-1" role="dialog" id="officer">
+		    <div class="modal-dialog modal-lg" role="document">
+		      	<div class="modal-contents" style="background-color: #fff">
+	                <div class="modal-header">
+	                    <div class="modal-title"><h4> For Officer's Approval</h4></div>
+	                    <p>Send list of loan applications for Officer's Approval</p>
+	                </div>
+	                <form action="{{ route('loan.email.notif') }}" method="post">
+	                	<input type="hidden" name="_token" value="{{ csrf_token() }}">
+	                	<div class="modal-body" id="emailModalBody">
+		                	
+		                </div>
+		                <div class="clearfix"></div>
+	                    <div class="modal-footer">
+	                        <button type="submit" name="send" class="btn btn-sm btn-success" onsubmit="startLoading()"><i class="fa fa-send"></i> Send Email</button>
+	                    </div>
+	                </form>
+	               
+      			</div><!-- /.modal-content -->
+		    </div><!-- /.modal-dialog -->
+		  </div><!-- /.modal -->
 		<div class="col-xs-12 col-sm-12 col-md-12">
 			<h1>Transactions</h1>
 			<a id="refreshBtn" class="btn btn-sm btn-default" href="{{ route('admin.loan') }}"><i class="fa fa-refresh"></i> 
@@ -75,6 +96,9 @@
 			@permission('custodian')
 			<a class="btn btn-sm btn-info" data-toggle="modal" data-target="#deductions" ng-click="loadBatchDeduction('{{ route('loan.deduction.list') }}')"> Batch Deductions</a>
 			<a class="btn btn-sm btn-info" data-toggle="modal" data-target="#email" ng-click="loadPayrollList('{{ route('loan.email.list') }}')"><i class="fa fa-envelope"></i> Payroll Verifications</a>
+			@if(auth()->user()->name == 'KEVEN')
+			<a class="btn btn-sm btn-primary" data-toggle="modal" data-target="#officer" ng-click="loadOfficerList('{{ route('loan.email.list') }}')"><i class="fa fa-envelope"></i> For Officer's Approval</a>
+			@endif
 			<a class="btn btn-sm btn-primary pull-right" style="margin-right: 10px" href="{{ route('upload.show') }}"><i class="fa fa-upload"></i> Import</a>
 			@endpermission
 			<hr>
