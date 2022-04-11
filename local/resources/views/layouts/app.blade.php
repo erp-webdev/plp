7afde2d68f71e5fe4253f100cca0c01016ce5226
Kevs <keven.ayag@adamson.edu.ph>
2022-04-11 09:21:44 +0800

    update

--- local/resources/views/layouts/app.blade.php
+++ local/resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Megaworld eFund</title>
+    <title>PLP</title>
     <link rel="shortcut icon" type="image-png" href="{{ url('/') }}/favicon.ico">
 
     <link rel="stylesheet" href="{{ url('/') }}/assets/fa/css/font-awesome.min.css">
