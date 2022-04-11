648b0baa0ff3017f09733a8b4d62ad7846232dbf
Kevs <keven.ayag@adamson.edu.ph>
2022-04-11 15:19:41 +0800

    update

--- local/app/Http/Controllers/admin/DashboardController.php
+++ local/app/Http/Controllers/admin/DashboardController.php
@@ -391,7 +391,7 @@ class DashboardController extends Controller
         $file = new LoanController();
         $filename = $file->printPDFForm($id);
 
-        $content = Storage::disk('forms')->get($file);
+        $content = Storage::disk('forms')->get($filename);
         
         return (new Response($content, 200))
             ->header('Content-Type', 'application/pdf');
