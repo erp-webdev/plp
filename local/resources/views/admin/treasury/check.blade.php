4ad54525005672844596c1f0f7666c5a08f2c94f
Kevs <keven.ayag@adamson.edu.ph>
2022-04-12 14:42:22 +0800

    test

--- local/resources/views/admin/treasury/check.blade.php
+++ local/resources/views/admin/treasury/check.blade.php
@@ -135,8 +135,8 @@
 
     <div class="input-group">
         <input type="date" class="form-control" name="check_release_date" required>
-        <div class="input-group-button">
-                <button type="submit" name="release" class="btn btn-success btn-sm" onsubmit="startLoading(); validate('release')"><i class="fa fa-send"></i> Release Check</button>
+        <div class="input-group-btn">
+                <button type="submit" name="release" class="btn btn-success " onsubmit="startLoading(); validate('release')"><i class="fa fa-send"></i> Release Check</button>
         </div>
     </div>
 
