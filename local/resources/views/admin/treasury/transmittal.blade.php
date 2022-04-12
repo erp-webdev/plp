f0982cf4bd29a0b23718fb04fda20e5f7dec6209
Kevs <keven.ayag@adamson.edu.ph>
2022-04-12 17:10:14 +0800

    treasury transmittal

--- local/resources/views/admin/treasury/transmittal.blade.php
+++ local/resources/views/admin/treasury/transmittal.blade.php
@@ -1,20 +1,20 @@
-<table>
+<table style="color: black; border: collapsed">
     <thead>
-        <th>Company</th>
-        <th>Name</th>
-        <th>CV #</th>
-        <th>Check #</th>
-        <th>Amount</th>
-        <th>Check Released Date</th>
+        <th style="border: 1px solid black; padding: 2px">Company</th>
+        <th style="border: 1px solid black; padding: 2px">Name</th>
+        <th style="border: 1px solid black; padding: 2px">CV #</th>
+        <th style="border: 1px solid black; padding: 2px">Check #</th>
+        <th style="border: 1px solid black; padding: 2px">Amount</th>
+        <th style="border: 1px solid black; padding: 2px">Check Released Date</th>
     </thead>
     <tbody>
         @foreach($loans as $loan)
         <tr>
-            <td>{{ $loan->COMPANY }}</td>
-            <td>{{ $loan->FullName }}</td>
-            <td>{{ $loan->cv_no }}</td>
-            <td>{{ number_format($loan->loan_amount, 2, '.', ',') }}</td>
-            <td>{{ $loan->released }}</td>
+            <td style="border: 1px solid black; padding: 2px">{{ $loan->COMPANY }}</td>
+            <td style="border: 1px solid black; padding: 2px">{{ $loan->FullName }}</td>
+            <td style="border: 1px solid black; padding: 2px">{{ $loan->cv_no }}</td>
+            <td style="border: 1px solid black; padding: 2px">{{ number_format($loan->loan_amount, 2, '.', ',') }}</td>
+            <td style="border: 1px solid black; padding: 2px">{{ $loan->released }}</td>
         </tr>
         @endforeach
     </tbody>
