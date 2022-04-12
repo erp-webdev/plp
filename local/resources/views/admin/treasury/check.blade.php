d1bf068c8d1237d25422b68554738eb928c9e294
Kevs <keven.ayag@adamson.edu.ph>
2022-04-12 14:41:57 +0800

    test

--- local/resources/views/admin/treasury/check.blade.php
+++ local/resources/views/admin/treasury/check.blade.php
@@ -127,21 +127,22 @@
   <div class="clearfix"></div>
   <div class="modal-footer">
 
-    @if(!empty($loan->cv_no))
-      <a href="{{ route('treasury.voucher.print', $loan->id) }}" class="btn btn-default btn-sm" target="_blank"><i class="fa fa-print"></i> Print Check Voucher</a>
-    @endif
+    
     {{-- <button type="submit" name="cancel" class="btn btn-danger btn-sm" onsubmit="startLoading(); validate('cancel')"><i class="fa fa-ban"></i> Cancel</button> --}}
     @if(!empty($loan->cv_no) && $loan->status == $utils->getStatusIndex('treasury'))
     <button type="submit" name="approve" class="btn btn-success btn-sm" onsubmit="startLoading(); validate('submit')"><i class="fa fa-send"></i> Submit</button>
     @elseif($loan->released == null && $loan->status == $utils->getStatusIndex('release'))
 
     <div class="input-group">
-      <input type="date" class="form-control" name="check_release_date" value="{{ date('m/d/Y') }}">
+        <input type="date" class="form-control" name="check_release_date" required>
         <div class="input-group-btn">
                 <button type="submit" name="release" class="btn btn-success btn-sm" onsubmit="startLoading(); validate('release')"><i class="fa fa-send"></i> Release Check</button>
         </div>
     </div>
 
+    @endif
+    @if(!empty($loan->cv_no))
+      <a href="{{ route('treasury.voucher.print', $loan->id) }}" class="btn btn-default btn-sm" target="_blank"><i class="fa fa-print"></i> Print Check Voucher</a>
     @endif
     <button type="button" class="btn btn-default btn-sm" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
 
