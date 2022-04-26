4b685a81c8a62d02d637822c77db5cb97e568582
Kevs <keven.ayag@adamson.edu.ph>
2022-04-26 16:20:00 +0800

    update

--- local/resources/views/admin/loans/form.blade.php
+++ local/resources/views/admin/loans/form.blade.php
@@ -18,7 +18,7 @@
         }
         .container {
             width: 816px;
-            height: 1248px;
+            height: 1344px;
             margin: 25px auto 0;
             border: 2px solid #111;
             background-color: #fff;
