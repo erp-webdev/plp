a02605f56f7bac883eaecd9085ccb5332601097e
Kevs <keven.ayag@adamson.edu.ph>
2022-04-26 11:31:04 +0800

    update

--- local/resources/views/admin/loans/loanApproval.blade.php
+++ local/resources/views/admin/loans/loanApproval.blade.php
@@ -240,9 +240,9 @@
 		     	@permission(['officer'])
 				@if($loan->status == $utils->getStatusIndex('officer'))
 				<div class="clearfix"></div>
-				<button type="submit" name="deny" class="btn btn-danger btn-sm pull-right" onsubmit="startLoading()"><i class="fa fa-thumbs-down"></i> Deny</button>
-				<button type="submit" name="approve" class="btn btn-success btn-sm pull-right" onsubmit="startLoading()"><i class="fa fa-thumbs-up"></i> Approve</button>
-				<button type="statusubmit" name="calculate" class="btn btn-default btn-sm pull-right" onsubmit="startLoading()"><i class="fa fa-calculator"></i> Calculate</button>
+				<button type="button" name="deny" class="btn btn-danger btn-sm pull-right btnSave" data-title="Confirm Disapproval" data-content="Are you sure you want to DISAPPROVE the application of {{ $loan->FullName }}?"><i class="fa fa-thumbs-down"></i> Deny</button>
+				<button type="button" name="approve" class="btn btn-success btn-sm pull-right btnSave" data-content="Are you sure you want to APPROVE the application of {{ $loan->FullName }}?"><i class="fa fa-thumbs-up"></i> Approve</button>
+				<button type="button" name="calculate" class="btn btn-default btn-sm pull-right btnSave" data-title="Confirm Recalculation" data-content="Are you sure you want to recalculate this loan application of {{ $loan->FullName }}?"><i class="fa fa-calculator"></i> Calculate</button>
 				@endif
 				@endif
 			</form>
