31bebe5ee88bdc8461e95381764f99791d028dab
Kevs <keven.ayag@adamson.edu.ph>
2022-05-16 13:18:49 +0800

    update

--- local/resources/views/admin/treasury/index.blade.php
+++ local/resources/views/admin/treasury/index.blade.php
@@ -40,7 +40,7 @@
 			<a class="btn btn-sm btn-primary" data-toggle="modal" data-target="#transmittal" ng-click="loadTransmittalList('{{ route('treasury.email.list') }}')"><i class="fa fa-envelope"></i> Transmittals</a>
 
 			<form action="" method="post">
-				{{ csrf_input() }}
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 				<div class="input-group">
 					<div class="input-group-addon">
