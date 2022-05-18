3c58051e654b116de35915b78f3a9d5cea741446
Kevs <keven.ayag@adamson.edu.ph>
2022-05-18 12:32:11 +0800

    upload data

--- local/resources/views/admin/loans/index.blade.php
+++ local/resources/views/admin/loans/index.blade.php
@@ -219,7 +219,7 @@
 						<th colspan="4">
 							Showing {{ $loans->count() }} of {{ $loans->total() }} of page {{ $loans->currentPage() }} of {{ $loans->lastPage() }}
 						</th>
-						<th colspan="9" class="text-align: align-right">
+						<th colspan="9" class="text-align: right">
 							{{ $loans->links() }}
 						</th>
 					</tfoot>
