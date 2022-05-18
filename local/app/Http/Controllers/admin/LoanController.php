3938c9257c712c170bca947b77e7dc45951e6cac
Kevs <keven.ayag@adamson.edu.ph>
2022-05-18 17:27:03 +0800

    upload data

--- local/app/Http/Controllers/admin/LoanController.php
+++ local/app/Http/Controllers/admin/LoanController.php
@@ -1055,7 +1055,7 @@ class LoanController extends Controller
 
             if(!empty(trim($cc))){
                 $ccs = explode(',', $cc);
-                $message->cc = $c[0];
+                $message->cc = $ccs[0];
                 foreach($ccs as $c)
                     if(!empty(trim($c)))
                         $message->cc = $c; 
