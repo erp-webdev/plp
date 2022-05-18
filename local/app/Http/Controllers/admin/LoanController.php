29165e001f1f4044cf0e9892b9e36cff0d31bbc6
Kevs <keven.ayag@adamson.edu.ph>
2022-05-18 10:36:57 +0800

    upload data

--- local/app/Http/Controllers/admin/LoanController.php
+++ local/app/Http/Controllers/admin/LoanController.php
@@ -696,6 +696,7 @@ class LoanController extends Controller
     
                 // Create deduction list or Ledger
                 $deductionDate = date('Y-m-d', strtotime($loan->startofdeductions));
+                $balance = $loan->balanceamount;
 
                 for($i = 0; $i < $loan->termsmonths * 2; $i++){
 
