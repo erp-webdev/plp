0023a7d56ab27189da71697e7c24067abbcb861f
Kevs <keven.ayag@adamson.edu.ph>
2022-05-20 13:35:22 +0800

    update

--- local/resources/views/admin/loans/upload.blade.php
+++ local/resources/views/admin/loans/upload.blade.php
@@ -61,7 +61,7 @@
 				<tbody>
 					@foreach($loans as $loan)
 					<tr>
-						<td style="width:200px"><? echo implode('<br>', $loan->errors); ?> *required fields</td>
+						<td style="width:200px">required fields <? echo implode(', ', $loan->errors); ?> *</td>
 						@foreach($loan->data as $key=>$value)
 						<td class="{{ in_array($key, $loan->errors) ? 'bg-warning' : '' }}">{{ $value }}</td>
 						@endforeach
