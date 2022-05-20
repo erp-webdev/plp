f3fee3c2e51b8a3eb7e1116958fdaa4068dfe0a6
Kevs <keven.ayag@adamson.edu.ph>
2022-05-20 11:11:33 +0800

    update

--- local/app/Http/Controllers/admin/LoanController.php
+++ local/app/Http/Controllers/admin/LoanController.php
@@ -665,7 +665,7 @@ class LoanController extends Controller
                 foreach($loan->toArray() as $key=>$value)  {
                     if(in_array($key, ['startofdeductions', 'balanceamount', 'deductionpercutoff', 
                         'totalpayable', 'interestpercent', 'interestpercent', 'loanamount', 'termsmonths', 
-                        'applicationdate']) && empty(trim($value))){
+                        'applicationdate']) && empty(trim($value)) && $key != '0'){
                             array_push($errors, $key .' is a required field');
                             $valid = false;
                             dd($key.'-'.$value);
