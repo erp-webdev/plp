ddcf1a7b6ac55bfc8a631159335d57ad4ab4ecec
Kevs <keven.ayag@adamson.edu.ph>
2022-05-30 14:47:32 +0800

    update

--- local/resources/views/admin/treasury/transmittal-show.blade.php
+++ local/resources/views/admin/treasury/transmittal-show.blade.php
@@ -34,7 +34,7 @@
         <div class="table-responsive">
             <table class="table table-sm table-hover table-bordered table-striped table-condensed">
                 <thead>
-                    <th style=""><input type="checkbox" onclick="checkBoxes()" title="Include to the email list" checked></th>
+                    <th style=""><input type="checkbox" onclick="checkBoxes()" title="If checked, include to the email list" checked></th>
                     <th style="">Company</th>
                     <th style="">Name</th>
                     <th style="">CV #</th>
@@ -45,7 +45,7 @@
                 <tbody>
                     @foreach($transmittals as $loan)
                     <tr>
-                        <td style=""><input type="checkbox" name="include[]" value="{{ $loan->id }}" checked></td>
+                        <td style=""><input type="checkbox" name="include[]" value="{{ $loan->id }}" title="If checked, include to the email list" checked></td>
                         <td style="">{{ $loan->COMPANY }}</td>
                         <td style="">{{ $loan->FullName }}</td>
                         <td style="">{{ $loan->cv_no }}</td>
