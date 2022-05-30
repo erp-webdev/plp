9cf9663f15d60200ca5bc0b81552f05080eaebb2
Kevs <keven.ayag@adamson.edu.ph>
2022-05-30 14:50:54 +0800

    update

--- local/resources/views/admin/treasury/transmittal-show.blade.php
+++ local/resources/views/admin/treasury/transmittal-show.blade.php
@@ -14,6 +14,7 @@
       
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <hr>
         <form action="{{ route('treasury.transmittal') }}" method="GET">
             {{ csrf_field() }}
             <div class="col-xs-12 col-sm-6 col-md-6">
