cf0c68ff7bf638b37efd78f555c6305b821f78f1
Kevs <keven.ayag@adamson.edu.ph>
2022-05-30 14:38:47 +0800

    update

--- local/resources/views/admin/treasury/transmittal-show.blade.php
+++ local/resources/views/admin/treasury/transmittal-show.blade.php
@@ -20,7 +20,7 @@
                         <input class="form-control input-sm datepicker-range" name="check_released" placeholder="mm/dd/yyyy - mm/dd/yyyy" value="{{ $_GET['check_released'] or old('check_released') }}" autocomplete="off">
                         <div class="input-group-btn">
                             <button class="btn btn-primary" type="submit" name="search"><i class="fa fa-search"></i></button>
-                            <a class="btn btn-default" href="{{ route('treasury.transmittal') }}">All</a>
+                            <a class="btn btn-default btn-sm" href="{{ route('treasury.transmittal') }}">All</a>
                         </div>
                         
                     </div>
