46b486a791070895ccb7508793b9184fa422e90c
Kevs <keven.ayag@adamson.edu.ph>
2022-05-30 14:15:04 +0800

    update

--- local/resources/views/admin/treasury/transmittal-show.blade.php
+++ local/resources/views/admin/treasury/transmittal-show.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12">
         <h1>Treasury Transmittals</h1>
-        <span>Send list of transmittals to PLP Custodian</span>
+        <h4>Send list of transmittals to PLP Custodian</h4>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12">
         <a href="{{ route('treasury.index') }}" class="btn btn-default btn-sm"><i class="fa fa-arrow-left"></i> Back</a>
