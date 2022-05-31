86f6ba1ce3e2db5471462e1ac5edcc135babb06a
Kevs <keven.ayag@adamson.edu.ph>
2022-05-31 13:40:13 +0800

    update

--- local/app/Http/Controllers/admin/ApplicationController.php
+++ local/app/Http/Controllers/admin/ApplicationController.php
@@ -749,7 +749,6 @@ class ApplicationController extends Controller
             ->first();
 
         if(!empty($allow_max_ex)){
-            dd($allow_max_ex);
             return false;
         }
 
