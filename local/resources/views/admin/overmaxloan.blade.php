ce8717cc17d9d230c4a0aa779dfc20e86a5fec84
Kevs <keven.ayag@adamson.edu.ph>
2022-05-31 10:38:06 +0800

    update

--- local/resources/views/admin/overmaxloan.blade.php
+++ local/resources/views/admin/overmaxloan.blade.php
@@ -8,7 +8,7 @@
             <h1>Above Maximum Loanable Amount</h1>
             <p>The list allows employee to submit PLP applications with loanable amount above their maximum limit. Each entry is applicable for one time use only and must be applied on or before the expiration date.</p>
             <div class="table-responsive">
-                <table class="table table-sm table-hover table-striped">
+                <table class="table table-sm table-hover table-striped dataTable">
                     <thead>
                         <th>ID</th>
                         <th>EmpID</th>
