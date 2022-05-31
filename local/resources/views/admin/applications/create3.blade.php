eb36baef75f405bb8e4a0abb20f2f0e2e9c67e13
Kevs <keven.ayag@adamson.edu.ph>
2022-05-31 13:19:15 +0800

    update

--- local/resources/views/admin/applications/create3.blade.php
+++ local/resources/views/admin/applications/create3.blade.php
@@ -306,7 +306,14 @@
                     </tr>
                     <tr>
                         @if(isset($loan->id))
-                        <td colspan="2" class=""><button type="submit" name="submit" class="btn btn-primary pull-right " data-validate="validate_standard" data-title="Submit Application!" data-content="Are you sure you want to submit application?"  data-form="#loanform">Submit</button></td>
+                        <td colspan="2" class="">
+                            
+                            <button type="button" class="btn btn-default" onclick="editForm()"><i class="fa fa-pencil"></i> Edit</button>
+                            <button type="submit" class="btn btn-primary hidden" name="submit" data-title="Verify and Continue Application!" data-content="Are you sure you want to verify and continue application?" data-form="#loanform">Continue</button>
+
+                            <button type="submit" name="submit" class="btn btn-primary pull-right " data-validate="validate_standard" data-title="Submit Application!" data-content="Are you sure you want to submit application?"  data-form="#loanform">Submit</button>
+                        
+                        </td>
                         @else
                         <td colspan="2" class=""><button type="submit" name="verify" class="btn btn-primary pull-right " data-title="Verify and Continue Application!" data-content="Are you sure you want to verify and continue application?" data-form="#loanform">Continue</button></td>
                         @endif
