b790f0505dc32a49c6f11d2b3556806f0057f122
Kevs <keven.ayag@adamson.edu.ph>
2022-05-31 11:26:54 +0800

    update

--- local/resources/views/admin/applications/create3.blade.php
+++ local/resources/views/admin/applications/create3.blade.php
@@ -206,15 +206,13 @@
                         <td>
                             <input type="number" name="loan_amount" class="form-control" 
                                     min="<?php echo $terms->min_loan_amount; ?>" 
-                                    max="
-                                        @if(!$allowed_above_max)
+                                    max="@if(!$allowed_above_max)
                                             @if(isset($loan->special)) 
                                                 {{ number_format($special->max_loan_amount, 2, '.', ',') }}
                                             @else
                                                 {{ number_format($terms->max_loan_amount, 2, '.', ',') }}
                                             @endif 
-                                        @endif
-                                        " 
+                                        @endif" 
                                     value="<?php 
                                         if(!empty($loan->loan_amount)){
                                             echo $loan->loan_amount;
