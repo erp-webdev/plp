f141a2b55b64cdceedcf389214651cf2519ad56d
Kevs <keven.ayag@adamson.edu.ph>
2022-05-31 10:42:24 +0800

    update

--- local/resources/views/admin/overmaxloan.blade.php
+++ local/resources/views/admin/overmaxloan.blade.php
@@ -51,3 +51,68 @@
 </form>
 
 @endsection
+@section('modals')
+<div id="SearchModal" class="modal fade-in">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                Search Employee
+                <span class="">
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="search" placeholder="Search..." autocomplete="off" class="form-control">
+                        <span class="input-group-btn">
+                            <button id="butotn" onclick="search()" class="btn btn-default"><i class="fa fa-search"></i> Search</button>
+                        </span>
+                    </div>
+                </span>
+            </div>
+            <div class="modal-body">
+                <table id="search_employee_table" class="table table-hover table-striped">
+                    <thead>
+                        <tr>
+                            <th>EmpID</th>
+                            <th>Name</th>
+                            <th>DBName</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+
+function setApprover(event){
+    $('input[name="EmpID"]').val($(event).data('empid'));
+    $('input[name="DBName"]').html($(event).data('name'));
+    $('input[name="FullName"]').val($(event).data('db'));
+
+    $('#SearchModal').modal('hide');
+}
+
+function search(){
+    $.ajax({
+        type: "get",
+        url: "{{ url('/getEmployee') }}",
+        data: {search: $('#search').val() },
+        success: function (response) {
+            $('#search_employee_table tbody').html('');
+
+            $.each(response, function (index, item) { 
+
+                $('#search_employee_table tbody').append(
+                '<tr onclick="setApprover(this)" data-empid="'+ item['EmpID'] +'" data-db="'+ item['DBNAME'] +'" data-name="'+ item['FullName'] +'">' +
+                    "<td>" + item['EmpID'] + "</td>" +
+                    "<td>" + item['FullName'] + "</td>" +
+                    "<td>" + item['DBName'] + "</td>" +
+                "</tr>"
+                );
+            });                 
+        }
+    });
+}
+</script>
+@endsection
