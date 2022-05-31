53bfd889494a4de9e4d4f1732ebd8ab685041799
Kevs <keven.ayag@adamson.edu.ph>
2022-05-31 10:57:09 +0800

    update

--- local/resources/views/admin/overmaxloan.blade.php
+++ local/resources/views/admin/overmaxloan.blade.php
@@ -31,7 +31,7 @@
                             <td><button type="submit" name="save" class="btn btn-primary" title="Save"><i class="fa fa-save"></i></button></td>
                         </tr>
                         @foreach($employees as $employee)
-                        <tr>
+                        <tr {{ date('Y-m-d', strtotime($employee->ExpiredAt)) >= date('Y-m-d') ? 'bg-red' : '' }}>
                             <td>
                                 <input type="hidden" name="id" value="{{ $employee->ID }}">
                                 {{ $employee->ID }}</td>
