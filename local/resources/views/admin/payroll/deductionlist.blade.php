026e4160f3d08e746cfea39a2fd09f018421e8ee
kevs-desktop <kayag.global@megaworldcorp.com>
2023-06-01 15:07:39 +0800

    update

--- local/resources/views/admin/payroll/deductionlist.blade.php
+++ local/resources/views/admin/payroll/deductionlist.blade.php
@@ -16,7 +16,7 @@
                     <td>{{ $emp->ctrl_no }}</td>
                     <td>{{ $emp->EmpID }}</td>
                     <td>{{ utf8_encode($emp->FullName) }}</td>
-                    <td>{{ date('Y-m-d', strtotime($emp->start_of_deduction)) }}</td>
+                    <td>{{ date('Y-m-d', strtotime($emp->start_of_deductions)) }}</td>
                     <td style="text-align: right;">{{ number_format($emp->deductions, 2, '.', ',') }}</td>
                     <td style="text-align: right;">{{ number_format($emp->total, 2, '.', ',') }}</td>
                 </tr>
